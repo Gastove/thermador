32a39a3db73827dc2e06b0455530780b06e4ed9f
Ross Donaldson <gastove@gmail.com>
2014-06-02 22:22:28 -0700

    w00t! Can create new pages from dropbox, and update existing pages. Hottest damn!

--- src/clj_server_test/models/page.clj
+++ src/clj_server_test/models/page.clj
@@ -18,9 +18,29 @@
 ;; TODO: Get this the hell out of this ns :-P
 ;; TODO: Need a "update text in DB" function.
 
+(defn get-all []
+  (sql/query DB ["SELECT * FROM pages"]))
+
+(defn prompt [word]
+  (println (str word "\n--> "))
+  (let [answer (read-line)]
+    (print answer)
+    answer))
+
+(defn print-then-prompt [message word]
+  (println message)
+  (prompt word))
+
 (defn add-page []
-  (let [prompt (fn [word] (println (str word "?"))(read-line))
-        name (prompt "Name")
+  (let [name (prompt "Name")
         path (prompt "Path")
         body (dbx/load-file-from-dbx path)]
     (sql/insert! DB :pages {:name name :body body})))
+
+(defn update-page []
+  (let [all-page-names (map :name (get-all))
+        page-list (reduce #(apply str %1 "-" %2 "\n") "" all-page-names)
+        page-name (print-then-prompt page-list "Update?")
+        path (prompt "Where's the new markdown")
+        new-body (dbx/load-file-from-dbx path)]
+    (sql/update! DB table-name {:body new-body} ["name = ?" page-name])))
