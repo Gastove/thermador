9480c7bc49f775a9a9915a98b7019688ecff97d4
Ross Donaldson <gastove@gmail.com>
2014-08-21 08:15:01 -0700

    Mostly WIP, but does tidy the ns a little.

--- src/thermador/data/proto.clj
+++ src/thermador/data/proto.clj
@@ -1,20 +1,26 @@
 (ns thermador.data.proto
-  (:refer-clojure :exclude [get extend]))
+  (:refer-clojure :rename {get core-get
+                           extend core-extend}))
 
 (defn beget
   [parent]
-  (assoc {} :prototype parent))
+  {:prototype parent})
 
 (defn get
-  [obj k]
-  (if-let [res (k obj)]
+  [pobj k]
+  (if-let [res (k pobj)]
     res
-    (if-let [proto (:prototype obj)]
+    (if-let [proto (:prototype pobj)]
       (get proto k)
       nil)))
 
 (defn extend
-  [obj extention]
+  [pobj extention]
   {:pre [(map? extention)
-         (contains? obj :prototype)]}
-  (into obj extention))
+         (contains? pobj :prototype)]}
+  (into pobj extention))
+
+(defn key-chain
+  "A key can exist in many levels of a prototype chain.
+  Returns a list of all the values for a key in Parent->Child order."
+  [pobj k])
