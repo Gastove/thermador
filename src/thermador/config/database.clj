32f833370937840a915e298d5e5689cfc1cf5c61
Ross Donaldson <gastove@gmail.com>
2014-08-20 22:41:24 -0700

    This.... will eventually configure redis.
    Unclear if it does that yet.

--- src/thermador/config/database.clj
+++ src/thermador/config/database.clj
@@ -1,4 +1,14 @@
 (ns thermador.config.database
-  (:require [environ.core :refer [env]]))
+  (:require [environ.core :refer [env]]
+            [taoensso.carmine :as carmine :refer [wcar]]))
 
 (def DB (or (env :database-url) "postgres://localhost:5432/thermador"))
+
+(def conn-props (or
+                 (heroku-configs)
+                 nil))
+
+(defmacro db
+  "Do something in the context of the configured DB"
+  [& body]
+  `(wcar conn-props ~@body))
