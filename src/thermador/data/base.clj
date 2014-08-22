2c7f75706382ab92a75ff885988cde93d013fc90
Ross Donaldson <gastove@gmail.com>
2014-08-21 18:56:12 -0700

    Add a transform function. It's... thin.

--- src/thermador/data/base.clj
+++ src/thermador/data/base.clj
@@ -1,9 +1,22 @@
 (ns thermador.data.base
+  (:require [thermador.config.database :as datastore]
+            [thermador.data.proto :as proto])
   (:import [org.joda.time DateTime DateTimeZone]))
 
 (defn now
   []
   (DateTime. DateTimeZone/UTC))
+
+(defn transform
+  "Applies a function to this proto object.
+
+  There's a lot that should happen to make sure an edit is
+  correct; that work will *eventually* happen here.
+  "
+  [pobj]
+  (let [key-vector (proto/key-chain pobj)
+        db-key (datastore/assemble-redis-key key-vector)]
+    (datastore/db set db-key pobj)))
 
 (def Base
   {:redis-key-name "Thermador"
