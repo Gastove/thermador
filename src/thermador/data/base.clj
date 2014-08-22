753bd1d58da11a70d9caacea4e55e202b0acbc2b
Ross Donaldson <gastove@gmail.com>
2014-08-22 16:50:37 -0700

    Ongoing work building up a way for these models to actually write to DB.

--- src/thermador/data/base.clj
+++ src/thermador/data/base.clj
@@ -1,6 +1,7 @@
 (ns thermador.data.base
   (:require [thermador.config.database :as datastore]
-            [thermador.data.proto :as proto])
+            [thermador.data.proto :as proto]
+            [taoensso.carmine :as carmine])
   (:import [org.joda.time DateTime DateTimeZone]))
 
 (defn now
@@ -11,13 +12,25 @@
   "Applies a function to this proto object.
 
   There's a lot that should happen to make sure an edit is
-  correct; that work will *eventually* happen here.
+  correct; that work will *eventually* happen here. Probably.
   "
   [pobj f]
   (let [new-pobj (f pobj)
         key-vector (proto/key-chain new-pobj)
         db-key (datastore/assemble-redis-key key-vector)]
     (datastore/db set db-key new-pobj)))
+
+(defn store-pobj
+  [pobj k]
+  (datastore/db carmine/set object-key pobj))
+
+(defn store-key
+  [set-key k]
+  (datastore/db carmine/sadd set-key k))
+
+(defn retrieve-pobj
+  [redis-key]
+  (datastore/db carmine/get redis-key))
 
 (def Base
   {:redis-key-name "Thermador"
--- src/thermador/data/page.clj
+++ src/thermador/data/page.clj
@@ -2,12 +2,34 @@
   (:require [thermador.data.proto :as proto]
             [thermador.data.base :as base]))
 
-(def -PageDefaults
+(def -PageChildFields
   {:title ""
    :id 0
    :body ""
-   :redis-key-name "Page"})
+   :redis-key-name ""})
+
+(declare create retrieve)
+(def -PageFields
+  {:redis-key-name "Page"
+   :create
+   :retrieve})
 
 (def Page
   (let [n (proto/beget base/Base)]
-    (proto/extend n -PageDefaults)))
+    (proto/extend n -PageFields)))
+
+(defn create
+  ""
+  ([])
+  ([fields]
+     (let [new-pobj (proto/beget Page)
+           obj-fields (merge -PageChildFields fields)
+           new-pobj (proto/extend new-pobj obj-fields)
+           key-parts (proto/key-chain new-pobj)
+           pobj-key (datastore/assemble-redis-key key-parts) ;This needs to go elsewhere.
+           parent-key-parts (proto/key-chain (:prototype new-pobj))
+           parent-key (datastore/assemble-redis-key parent-key-parts)]
+       (base/store-pobj new-pobj pobj-key)
+       (base/store-key parent-key pobj-key))))
+
+(defn retrieve [])
