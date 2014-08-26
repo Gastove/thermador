a665fb2edd106c499146a8c43c27db49b8053aa2
Ross Donaldson <gastove@gmail.com>
2014-08-26 09:49:38 -0700

    Do a butt-ton of work in `page`. Creating functions to actually do lookups and REST returns. The up side is that I think they're comparatively neat; the downside is that this is a *terrible* namespace for them to live in, and I need to reorganize.

--- src/thermador/data/page.clj
+++ src/thermador/data/page.clj
@@ -1,11 +1,15 @@
 (ns thermador.data.page
-  (:require [thermador.data.model :as model]))
+  (:require [thermador.data.model :as model]
+            [compojure.core :refer [defroutes GET]]
+            [cheshire.core :refer [generate-string]]))
 
 (def -PageEntityFields
   {:title ""
-   :id 0
+   ;;   :id 0
    :body ""
-   :datum-name ""})
+   :name ""
+   :datum-name ""
+   :created-on (model/now)})
 
 (def -PageModelFields
   {:datum-name "Page"})
@@ -15,3 +19,39 @@
 (defn create-page
   [fields]
   (model/create Page -PageEntityFields fields))
+
+(defmulti make-return
+  (fn [d]
+    (cond
+     (vector? d) :many
+     (map? @d) :single)))
+(defmethod make-return :single
+  [pobj]
+  (let [pobj-val (@pobj)
+        ret-obj (dissoc pobj-val :datum-name :prototype)
+        body (generate-string ret-obj)
+        status 200
+        headers {"Content-Type" "text/html"}]
+    {:status status
+     :headers headers
+     :body body}))
+(defmethod make-return :many
+  [pobjects]
+  (let [ret-objects (into [] (for [pobj pobjects
+                                   :let [pobj-val @pobj
+                                         ret-obj (dissoc pobj-val :datum-name :prototype)]]
+                               ret-obj))
+        body (generate-string ret-objects)
+        status 200
+        headers {"Content-Type" "text/html"}]
+    {:status status
+     :headers headers
+     :body body}))
+(defmethod make-return :default
+  [pobj]
+  {:status 404
+   :headers {"Content-Type" "text/html"}})
+
+(defroutes routes
+  (GET "/:id" [id] (make-return (model/retrieve :lookup-id :datum-name Page id)))
+  (GET "/" [] (make-return (model/retrieve :all :datum-name Page))))
