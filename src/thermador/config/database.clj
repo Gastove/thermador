ad701a210a8918d112161e376b7914b81c1df2ee
Ross Donaldson <gastove@gmail.com>
2014-08-26 19:55:32 -0700

    Try a different config for localhost, see if Travis likes it better.

--- src/thermador/config/database.clj
+++ src/thermador/config/database.clj
@@ -4,7 +4,7 @@
 
 (def DB (or (env :database-url) "postgres://localhost:5432/thermador"))
 
-(def conn-props {})
+(def conn-props {:pool {} :spec {:host "localhost" :port 6379}})
 
 (defmacro db
   "Do something in the context of the configured DB"
