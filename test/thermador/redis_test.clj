a4f41153e920687eab63ab614ec3540013f943e0
Ross Donaldson <gastove@gmail.com>
2014-08-26 19:32:04 -0700

    Tweak redis test

--- test/thermador/redis_test.clj
+++ test/thermador/redis_test.clj
@@ -3,8 +3,5 @@
             [taoensso.carmine :as carmine]
             [thermador.config.database :as datastore]))
 
-(deftest placeholder-test
-  (is true "I hope true is true"))
-
 (deftest redis-connection
   (is (= "PONG" (datastore/db (carmine/ping))) "Do we have ping?"))
