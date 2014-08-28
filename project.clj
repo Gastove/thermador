5e418bbe12d6ef1767ee90f477821cd1f41e9ee6
Ross Donaldson <gastove@gmail.com>
2014-08-27 19:14:44 -0700

    Add an exclusion

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thermador "1.0.0-SNAPSHOT"
+(defproject thermador "0.1.0"
   :description "Personal Server and Projects"
   :url "http://thermador.herokuapp.com"
   :dependencies [[org.clojure/clojure "1.6.0"]
@@ -12,11 +12,10 @@
                  [cheshire "5.3.1"]
                  [environ "0.5.0"]
                  [com.cemerick/drawbridge "0.0.6"]
-                 [com.dropbox.core/dropbox-core-sdk "1.7.4"]
+                 [com.dropbox.core/dropbox-core-sdk "1.7.4"
+                  :exclusions [com.fasterxml.jackson.core/jackson-core]]
                  [com.taoensso/carmine "2.6.2"]
-                 [joda-time/joda-time "2.4"]
-                 ;[midje "1.6.3"]
-                 ]
+                 [joda-time/joda-time "2.4"]]
   :min-lein-version "2.0.0"
   :plugins [[lein-environ "0.5.0"]
             [lein-ring "0.8.11"]]
