f368b59c2a94048b574aee56e4124d338872b228
Ross Donaldson <gastove@gmail.com>
2014-08-27 20:06:42 -0700

    Config redis for heroku

--- src/thermador/config/database.clj
+++ src/thermador/config/database.clj
@@ -4,7 +4,8 @@
 
 (def DB (or (env :database-url) "postgres://localhost:5432/thermador"))
 
-(def conn-props {:pool {} :spec {:host "localhost" :port 6379}})
+(def conn-props {:pool {}
+                 :spec {:host (or (env :rediscloud-url) "localhost") :port 6379}})
 
 (defmacro db
   "Do something in the context of the configured DB"
