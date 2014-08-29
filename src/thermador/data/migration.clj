255156773f7689656d7eee5c1f125adc1a13d6f9
Ross Donaldson <gastove@gmail.com>
2014-08-28 21:20:10 -0700

    Actually write a migration, which seeeeeems to work?

--- src/thermador/data/migration.clj
+++ src/thermador/data/migration.clj
@@ -1,22 +1,21 @@
 (ns thermador.data.migration
-  (:require [thermador.data.migration.page :as page]
+  (:require [thermador.data.model :as model]
+            [thermador.data.migration.page :as page]
+            [thermador.config.dropbox :as dbx]
             [clojure.string :as string]))
 
-(def- migration-map {})
-
-(defn sync-redis
-  [sync-map]
-  (for [[model data] sync-map
-        :let [candidate-pairs (dbx/list-files-in-folder (:path data))]]
-    (doseq [[id path] candidate-pairs
-            :let [{:keys [lookup-key candidate-model create-fn]} data]
-            :when (nil? (model/retrieve :lookup-id lookup-key candidate-model id))]
-      (create-fn (dbx/load-file-from-dbx path)))))
-
-(defn make-title-from-md
-  [md]
-  (string/trim (second (re-find #"([\w ]+)" md))))
+(def sync-map (into {} [page/page-migration]))
 
 (defn make-id-from-file-name
   [file-name]
   (first (string/split file-name #"\.")))
+
+(defn sync-redis
+  []
+  (for [[sync-model data] sync-map
+        :let [candidate-pairs (dbx/list-files-in-folder (:path data))]]
+    (doseq [[file-name path] candidate-pairs
+            :let [id (make-id-from-file-name file-name)
+                  {:keys [lookup-key create-fn]} data]
+            :when (nil? (model/retrieve :lookup-id lookup-key sync-model id))]
+      (create-fn id (dbx/load-file-from-dbx path)))))
--- src/thermador/data/migration/page.clj
+++ src/thermador/data/migration/page.clj
@@ -1,4 +1,17 @@
 (ns thermador.data.migration.page
-  (:require [thermador.data.page :as page-model]))
+  (:require [thermador.data.page :as page]
+            [clojure.string :as string]))
 
-(def page-migration {})
+(defn make-title-from-md
+  [md]
+  (string/trim (second (re-find #"([\w !?]+)" md))))
+
+(defn create-page-from-markdown-text
+  [name markdown]
+  (let [title (make-title-from-md markdown)]
+    (page/create-page name title markdown)))
+
+(def page-migration
+  {page/Page {:path "/the_range/markdown"
+              :lookup-key page/lookup-key
+              :create-fn create-page-from-markdown-text}})
