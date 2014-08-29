638867c14010c750da4d5fb519419098a477c924
Ross Donaldson <gastove@gmail.com>
2014-08-28 22:17:31 -0700

    Add a make-rest-return method, and a method for handling Joda datetimes for rest returns. I dislike this, but it's ok for now.

--- src/thermador/data/page.clj
+++ src/thermador/data/page.clj
@@ -1,8 +1,8 @@
 (ns thermador.data.page
-  (:require ;; [thermador.rest :as rest-api]
-            [thermador.data.model :as model]
+  (:require [thermador.data.model :as model]
             [compojure.core :refer [defroutes GET]]
-            [cheshire.core :refer [generate-string]]))
+            [cheshire.core :refer [generate-string]])
+  (:import [org.joda.time.format DateTimeFormat]))
 
 (def -PageEntityFields
   {:title ""
@@ -27,6 +27,19 @@
                    :body body
                    :datum-name datum-name}]
        (create-page fields))))
+
+(defn format-datetime
+  [dt]
+  (let [formatter (DateTimeFormat/forPattern "yyyy-MM-dd HH:mm:ss")]
+    (.print formatter dt)))
+
+(defn make-rest-return
+  [pobj]
+  (let [data @pobj
+        created-on (format-datetime (:created-on data))
+        ret-obj (select-keys data [:title :body :name])
+        ret-obj (assoc ret-obj :created-on created-on)]
+    ret-obj))
 
 ;; (defroutes page-routes
 ;;   (GET "/:id" [id] (rest-api/make-return (model/retrieve :lookup-id :datum-name Page id)))
