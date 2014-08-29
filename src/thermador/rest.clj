c6aefc1729b4c7f66695eeb41484f3b810482ff8
Ross Donaldson <gastove@gmail.com>
2014-08-28 22:17:57 -0700

    Retool this in a good way -- stop making the REST API deal with atoms.

--- src/thermador/rest.clj
+++ src/thermador/rest.clj
@@ -8,24 +8,18 @@
   (fn [d]
     (cond
      (vector? d) :many
-     (map? @d) :single)))
+     (map? d) :single)))
 (defmethod make-return :single
-  [pobj]
-  (let [pobj-val @pobj
-        ret-obj (dissoc pobj-val :datum-name :prototype)
-        body (generate-string ret-obj)
+  [ret-obj]
+  (let [body (generate-string ret-obj)
         status 200
         headers {"Content-Type" "text/html"}]
     {:status status
      :headers headers
      :body body}))
 (defmethod make-return :many
-  [pobjects]
-  (let [ret-objects (into [] (for [pobj pobjects
-                                   :let [pobj-val @pobj
-                                         ret-obj (dissoc pobj-val :datum-name :prototype)]]
-                               ret-obj))
-        body (generate-string ret-objects)
+  [ret-objects]
+  (let [body (generate-string ret-objects)
         status 200
         headers {"Content-Type" "text/html"}]
     {:status status
@@ -40,11 +34,15 @@
 (defmethod api-result "page"
   [resource & args]
   (if (empty? args)
-    (let [models (model/retrieve :all :datum-name page/Page)]
-      (make-return models))
+    (let [models (model/retrieve :all :datum-name page/Page)
+          returnable-models (into [] (map page/make-rest-return models))]
+      (make-return returnable-models))
     (case (first args)
-            "get" (make-return (model/retrieve :lookup-id (second args))))))
+      "get" (let [id (second args)
+                  pobj (model/retrieve :lookup-id :datum-name page/Page id)
+                  ret-obj (page/make-rest-return pobj)]
+                (make-return ret-obj)))))
 
 (defroutes rest-routes
   (GET "/:resource/:id" [resource id] (api-result resource "get" id))
-  (GET "/:resource/" [resource] (api-result resource)))
+  (GET "/:resource" [resource] (api-result resource)))
