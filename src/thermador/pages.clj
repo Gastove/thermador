ba027a6939d89271c316ffb658b40b90d0a2db12
Ross Donaldson <gastove@gmail.com>
2014-08-30 23:33:50 -0700

    Disable thermador/page

--- src/thermador/pages.clj
+++ src/thermador/pages.clj
@@ -4,23 +4,23 @@
             [cheshire.core    :refer [generate-string]]
             ))
 
-(def resource-map {:home  ["md", "markdown"]
-                   :about ["md", "markdown"]
-                   :cv    ["md", "markdown"]})
+;; (def resource-map {:home  ["md", "markdown"]
+;;                    :about ["md", "markdown"]
+;;                    :cv    ["md", "markdown"]})
 
-(defn make-file-path [id, r-map]
-  (let [[[folder, extention]] (r-map id)
-        file-name ()]
-    (apply str [folder file-name "." extention])
-    ))
+;; (defn make-file-path [id, r-map]
+;;   (let [[[folder, extention]] (r-map id)
+;;         file-name ()]
+;;     (apply str [folder file-name "." extention])
+;;     ))
 
-(defn load-file [file-path]
-  (slurp (resource file-path)))
+;; (defn load-file [file-path]
+;;   (slurp (resource file-path)))
 
 ;; (defn make-return
 ;;   [& :keys [id text]]
 ;;   (generate-string {:id id :text text}))
 
-(defroutes routes
-  (ANY "/" [])
-  (GET "/:id" [id]))
+;; (defroutes routes
+;;   (ANY "/" [])
+;;   (GET "/:id" [id]))
